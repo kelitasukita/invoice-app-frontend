1414c450351e6a31f10d17f02c46c276020bc2dd
Kelita Lamar <kelitasukita@gmail.com>
2023-04-14 13:59:22 +0200

    Set avatar styles

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,15 +2,16 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-center items-center bg-nav-bg-light">
-      <div>
+    <nav className="flex justify-between items-center bg-nav-bg-light">
+      <div >
         <Image src="/assets/logo-nav.png" width="72" height="72" alt="logotipo" />
       </div>
       <div>
         <Image src="/assets/icon-moon.svg" width="20" height="20" alt="logotipo" />
       </div>
       <div>
-        <Image src="/assets/image-avatar.jpg" width="32" height="32" alt="logotipo" />
+        <Image src="/assets/image-avatar.jpg" width="32" height="32" alt="logotipo"
+          className="rounded-full mr-6" />
       </div>
     </nav>
   )
