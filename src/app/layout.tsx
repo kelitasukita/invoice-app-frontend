af9e6005ca81819418ea05eb5a0dd1ef9b734efa
Kelita Lamar <kelitasukita@gmail.com>
2023-04-16 01:43:49 +0200

    Changes content from layout.tsx to page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='bg-11' >
-        <Navbar />
-        <main className='px-6 py-8 sm:px-12 sm:py-16'>
-          <AddInvoice />
-        </main>
+
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import AddInvoice from "./components/AddInvoice";
+import Navbar from "./components/Navbar";
 
 
 export default function Home() {
-  return
+  return (
+    <>
+      <Navbar />
+      <main className='px-6 py-8 sm:px-12 sm:py-16'>
+        <AddInvoice />
+      </main>
+    </>
+  )
 }
