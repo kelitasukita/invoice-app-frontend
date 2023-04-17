1222791c2724b23a83f0b4373366ddc1831631c1
Kelita Lamar <kelitasukita@gmail.com>
2023-04-17 22:08:48 +0200

    Rewrite Invoice component with display grid

--- src/app/components/Invoice.tsx
+++ src/app/components/Invoice.tsx
@@ -1,27 +1,43 @@
 export default function Invoice() {
   return (
-    <article className="desktop:mx-80 sm:shadow-my-shadow">
-      <div className="flex justify-between items-center mt-8 desktop:mt-14 bg-13 rounded-lg h-32 p-6">
-        <div className="flex-col">
-          <h2 className="heading-s text-08 mb-6">
-            <span className="text-07">#</span>
-            RT3080
-          </h2>
-          <span className="text-06 body inline-block mb-2">Due 19 Aug 2021</span>
-          <div>
-            <strong className="text-08 heading-s">$1,800.90</strong>
-          </div>
-        </div>
+    // <article className="desktop:mx-80 sm:shadow-my-shadow">
+    //   <div className="flex justify-between items-center mt-8 desktop:mt-14 bg-13 rounded-lg h-32 p-6">
+    //     <div className="flex-col">
+    //       <h2 className="heading-s text-08 mb-6">
+    //         <span className="text-07">#</span>
+    //         RT3080
+    //       </h2>
+    //       <span className="text-06 body inline-block mb-2">Due 19 Aug 2021</span>
+    //       <div>
+    //         <strong className="text-08 heading-s">$1,800.90</strong>
+    //       </div>
+    //     </div>
 
-        <div>
-          <div className="mb-11">
-            <span className="text-07 body float-right">Jensen Huang</span>
-          </div>
-          <div className="flex justify-center items-center bg-paid py-4 px-8 rounded-md bg-opacity-4">
-            <div className="w-2 h-2 rounded-full bg-paid"></div>
-            <span className="heading-s text-paid ml-1.5">Paid</span>
-          </div>
-        </div>
+    //     <div>
+    //       <div className="mb-11">
+    //         <span className="text-07 body float-right">Jensen Huang</span>
+    //       </div>
+    //       <div className="flex justify-center items-center bg-paid py-4 px-8 rounded-md bg-opacity-4">
+    //         <div className="w-2 h-2 rounded-full bg-paid"></div>
+    //         <span className="heading-s text-paid ml-1.5">Paid</span>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </article>
+
+    <article className="grid grid-cols-3 grid-rows-3 mt-8 h-32 p-6 bg-13 rounded-lg desktop:mx-80 sm:shadow-my-shadow">
+      <h2 className="heading-s text-08">
+        <span className="text-07 ">#</span>
+        RT3080
+      </h2>
+      <span className="col-start-1 col-end-2 text-06 body inline-block mt-4">Due 19 Aug 2021</span>
+      <strong className="col-start-1 col-end-2 text-08 heading-s mt-4">$1,800.90</strong>
+
+      <span className="col-start-3 col-end-3 row-start-1 text-right text-07 body">Jensen Huang</span>
+
+      <div className="col-start-3 col-end-3 row-start-2 row-end-3 flex justify-self-end justify-center items-center w-103 py-5 mt-4 bg-paid rounded-md bg-opacity-4">
+        <div className="w-2 h-2 rounded-full bg-paid"></div>
+        <span className="heading-s ml-2 rounded-md text-paid">Paid</span>
       </div>
     </article>
   )
