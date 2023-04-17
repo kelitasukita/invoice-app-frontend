5682ac849e3cd9503db87311511c100a12cc21ed
Kelita Lamar <kelitasukita@gmail.com>
2023-04-17 13:48:25 +0200

    Descomment Invoice component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           <Navbar />
           <main className='px-6 py-8 sm:px-12 sm:py-16 desktop:w-screen'>
             <AddInvoice />
-            {/* <Invoice /> */}
+            <Invoice />
           </main>
         </div>
         {children}
